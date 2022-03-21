905787697fb451e2f64b7101414905287c2adab8
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2022-03-21 16:28:45 +0100

    Update generate_extension.sh

--- generate_extension.sh
+++ generate_extension.sh
@@ -22,5 +22,5 @@ sudo chown -R root:root $TMPDIR2/zerofree/*
 cd $TMPDIR2/ && sudo mksquashfs zerofree zerofree.tcz -noappend
 sudo chown -R tc $TMPDIR2/zerofree/usr
 cp -R $TMPDIR2/ /out/
-chmod -R 777 /out
+sudo chmod -R 777 /out
 printf "\nzerofree TCZ extension successfully created\n"
